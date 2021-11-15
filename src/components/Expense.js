f5c61dc19d95df331687af5b5aafd1021e109953
FIRST_NAME LAST_NAME Deepa <deepapandey364@gmail.com>
2021-11-15 19:53:34 +0530

    expense tracker

---
+++ src/components/Expense.js
@@ -0,0 +1,31 @@
+import "./Expense.css";
+import ExpenseItem from "./ExpenseItem";
+
+function Expense(props) {
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        date={props.expenses[0].date}
+        title={props.expenses[0].title}
+        amount={props.expenses[0].amount}
+      />
+      <ExpenseItem
+        date={props.expenses[1].date}
+        title={props.expenses[1].title}
+        amount={props.expenses[1].amount}
+      />
+      <ExpenseItem
+        date={props.expenses[2].date}
+        title={props.expenses[2].title}
+        amount={props.expenses[2].amount}
+      />
+      <ExpenseItem
+        date={props.expenses[3].date}
+        title={props.expenses[3].title}
+        amount={props.expenses[3].amount}
+      />
+    </div>
+  );
+}
+
+export default Expense;
